c73137abe62085503bd50a3402a5826975b90ca2
Muh Fachrul <muhfachrul720@gmail.com>
2020-09-24 09:28:36 +0700

    Database Change 1

---
+++ application/models/M_alur.php
@@ -0,0 +1,40 @@
+<?php
+
+    class M_alur extends CI_Model
+    {
+        // Construct
+        public function __construct()
+        {
+            parent::__construct();
+        }
+
+        // General
+        public function insert($table, $data)
+        {
+            $this->db->insert($table, $data);
+            return $this->db->insert_id();
+        }
+
+        public function update($table, $where, $data)
+        {
+            $this->db->where($where);
+            return $this->db->update($table, $data);
+        }
+
+        public function delete($table, $where)
+        {
+            $this->db->where($where);
+            return $this->db->delete($table);
+        }
+
+        // Get
+        public function get_receiver($level)
+        {
+            $this->db->select('al.*');
+            $this->db->from('tbl_alur_berkas as al');
+            $this->db->where('level_sender', $level); 
+            return $this->db->get();
+        }
+    }
+
+    ?>
